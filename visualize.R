9a484b460231468e35dc8589f681aa9480c2a39a
golikp <46810318+golikp@users.noreply.github.com>
2023-06-13 12:52:02 +0200

    Add files via upload

--- visualize.R
+++ visualize.R
@@ -1,16 +1,3 @@
-#Read table with results for K = 3
-tbl <- read.table("EuPop.3.Q")
-indTable<-read.table("EuPop.fam", col.names=c("Pop","Sample","c1","c2","c3","c4"))
-merged=cbind(tbl, indTable)
-
-#Sort by population
-orderedPop = merged[order(merged$Pop),]
-
-#Sort by ancestral proportions
-orderedQ1 = merged[order(merged$V1),]
-orderedQ2 = merged[order(merged$V2),]
-orderedQ3 = merged[order(merged$V3),]
-
 barNaming <- function(vec) { #Do not repeat the same label
 	retVec <- vec
 	for (k in 2:length(vec)) {
@@ -25,20 +12,22 @@ plot_admix <- function(q_table, K) {
 	text(mp, par("usr")[3], labels = barNaming(q_table$Pop), srt = 45, adj = c(1.1,1.1), xpd = TRUE, cex=.2)	
 }
 
-pdf(file="plotPop.pdf", 20,4)
-plot_admix(orderedPop, 3)
+K <- 3 
+basename <- "EuAs"
+tblName <- paste(basename, as.character(K), "Q", sep = ".")
+famName <- paste(basename, "fam",  sep = ".")
+plotName <- paste("plot", as.character(K), "pdf", sep = ".")
+
+tbl <- read.table(file = tblName)
+indTable<-read.table(file = famName, col.names = c("Pop","Sample","c1","c2","c3","c4"))
+merged=cbind(tbl, indTable)
+
+#Sort by population
+orderedPop = merged[order(merged$Pop),]
+
+#Plot
+pdf(file = plotName, 20,4)
+	plot_admix(orderedPop, K)
 dev.off()
 
-pdf(file="plotQ1.pdf", 20,4)
-plot_admix(orderedQ1, 3)
-dev.off()
 
-pdf(file="plotQ2.pdf", 20,4)
-plot_admix(orderedQ2, 3)
-dev.off()
-
-pdf(file="plotQ3.pdf", 20,4)
-plot_admix(orderedQ3, 3)
-dev.off()
-
-#TODO: use a variable and loops to make it work for any K value not just 3
